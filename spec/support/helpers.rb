770c77d537cf02ab3605ca010e0cafc5140f4552
David Rodríguez <deivid.rodriguez@riseup.net>
2019-08-02 13:08:16 +0200

    Try fix last failure

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -110,7 +110,7 @@ module Spec
       bundle_bin = options.delete("bundle_bin") || bindir.join("bundle")
 
       if system_bundler = options.delete(:system_bundler)
-        bundle_bin = system_gem_path.join("bin/bundler")
+        bundle_bin = default_bundle_path("bin/bundler")
       end
 
       env = options.delete(:env) || {}
